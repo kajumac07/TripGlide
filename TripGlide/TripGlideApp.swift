e58e9c791ca249dcb24e2ee1e7aa47ab6daad2f0
kajumac07 <kajumac13@gmail.com>
2025-12-11 19:00:44 +0530

    Initial Commit

---
+++ TripGlide/TripGlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripGlideApp.swift
+//  TripGlide
+//
+//  Created by Atul Tiwari on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TripGlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
